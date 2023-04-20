c3f94cc1241cae4a9ef11f3d0240cf010b24fff7
Alejandro Mendieta <alejandro777kgb@gmail.com>
2023-04-20 14:49:42 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,34 @@
+"use strict"
+
+const botoesCarrossel = document.querySelectorAll('.botao');
+const imagens = document.querySelectorAll('.imagem');
+
+botoesCarrossel.forEach((botao, indice) => {
+    botao.addEventListener('click', () => {
+        desativarBotaoSelecionado();
+
+        selecionarBotaoCarrosel(botao);
+
+        esconderImagemAtiva();
+
+        mostrarImagem(indice);
+    })
+});
+
+function desativarBotaoSelecionado() {
+    const botaoSelecionado = document.querySelector('.selecionado');
+    botaoSelecionado.classList.remove('selecionado');
+};
+
+function selecionarBotaoCarrosel(botao) {
+    botao.classList.add('selecionado');
+}
+
+function esconderImagemAtiva() {
+    const imagemAtiva = document.querySelector('.ativa');
+    imagemAtiva.classList.remove('ativa');
+}
+
+function mostrarImagem(indice) {
+    imagens[indice].classList.add('ativa');
+}
